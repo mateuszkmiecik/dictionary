30425f04a7edf7634baa561d74596f42810a17b9
Mateusz Kmiecik <mk.chomik@gmail.com>
2013-06-03 10:26:03 +0200

    added ajax search, fixed search engine bugs

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,13 +4,41 @@ $.ajaxSetup ({
 });
 $(function() {
 
-	var searching = false;
+	var searching = false, query;
 
-	// $('#search').on({
-	// 	'keydown' : function () {
+	$('#search').keyup(function(){
+		query = $('#search').val();
+		searching = true;
+		$('#content').slideUp(400, function(){
+			$(this).children().remove();
+			$(this).wrap('<div class="fake"></div>');
+			$(this).remove();
+			$('.fake').load(window.location.pathname+'/search #content', {search: query}, showContent());
+		});
+	});
+
+	var showContent = function(){
+		$('.fake').hide();
+		$('.fake').slideDown(300);
+	}
+
+	// $('#search').bind({
+	// 	'input' : function () {
 	// 		searching = true;
-	// 		$('#content .row').not(':first-child').fadeOut(400);
-	// 		console.log(window.location.pathname);
+	// 		query = $('#search').val();
+
+	// 			$('#content *').fadeOut(400, function(){
+	// 				$.ajax({
+	// 					url: window.location.pathname+'/search',
+	// 					data: { search: query },
+	// 					success: function(data){
+	// 						var page = $.parseHTML(data);
+	// 						var content = $(page).children('#content').html();
+	// 						console.log(content);
+	// 						$('#content').html(content);
+	// 					}
+	// 				})
+	// 			});
 	// 	}
 	// });
 
